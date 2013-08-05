0f1252780e8fedaf4b17aa4797bc24e52f69d962
Scott Rabin <scottrabin@gmail.com>
2013-08-04 19:10:54 -0700

    Replace directly-bound listeners with a proxy version
    This sets up for allowing delegated event listeners to directly modify
    the `target` attribute, preserves and normalizes `defaultPrevented` on
    the source event, and allows for custom events to set the event type
    should the need arise to alias events.

---
+++ test/specs/dom/events/proxy.js
@@ -0,0 +1,55 @@
+"use strict";
+
+define(function(require) {
+	var EventProxy = require('dom/events/proxy');
+
+	describe("dom/events/proxy", function() {
+		beforeEach(function() {
+			this.source = {
+				bubbles: true,
+				cancelable: true,
+				currentTarget: jasmine.createElement('div'),
+				detail: {},
+				// eventPhase: null,
+				target: jasmine.createElement('span'),
+				timeStamp: new Date().getTime(),
+				type: 'eventType',
+				preventDefault: jasmine.createSpy('preventDefault'),
+				stopPropagation: jasmine.createSpy('stopPropagation')
+			};
+			this.proxy = EventProxy(this.source);
+		});
+
+		it("should copy properties from the source object", function() {
+			expect(this.proxy.bubbles).toBe(true);
+			expect(this.proxy.cancelable).toBe(true);
+			expect(this.proxy.currentTarget).toBe(this.source.currentTarget);
+			expect(this.proxy.detail).toBe(this.source.detail);
+			expect(this.proxy.target).toBe(this.source.target);
+			expect(this.proxy.timeStamp).toBe(this.source.timeStamp);
+			expect(this.proxy.type).toBe(this.source.type);
+		});
+
+		describe("#preventDefault", function() {
+			it("should call `preventDefault` on the source object", function() {
+				this.proxy.preventDefault();
+
+				expect(this.source.preventDefault).toHaveBeenCalled();
+			});
+
+			it("should set the `defaultPrevented` property to `true`", function() {
+				this.proxy.preventDefault();
+
+				expect(this.proxy.defaultPrevented).toBe(true);
+			});
+		});
+
+		describe("#stopPropagation", function() {
+			it("should call `stopPropagation` on the source object", function() {
+				this.proxy.stopPropagation();
+
+				expect(this.source.stopPropagation).toHaveBeenCalled();
+			});
+		});
+	});
+});
